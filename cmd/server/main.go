a439a7dda2f24b1895a3dc15efdb3242ac17e658
Kilovoltov <kilovoltov@yandex.ru>
2025-11-15 23:11:46 +0500

    fixup

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,34 +1,35 @@
 package main
 
 import (
-    "fmt"
-    "net/http"
-    "github.com/kilovoltov/kilometrix/internal/repository"
-    "github.com/kilovoltov/kilometrix/internal/handlers"
+	"fmt"
+	"net/http"
 
-    "github.com/go-chi/chi/v5"
+	"github.com/kilovoltov/kilometrix/internal/handlers"
+	"github.com/kilovoltov/kilometrix/internal/repository"
+
+	"github.com/go-chi/chi/v5"
 )
 
 func main() {
 	parseFlags()
 	if err := LoggerInitialize(logLevel); err != nil {
-        panic(err)
-    }
-    memStor := repository.NewMemStorage()
-    stor := handlers.NewStor(memStor)
+		panic(err)
+	}
+	memStor := repository.NewMemStorage()
+	stor := handlers.NewStor(memStor)
 
-    r := chi.NewRouter()
-    r.Post("/update/{metricType}/{metricName}/{metricValue}", requestLogger(stor.HandleMetricUpdate))
+	r := chi.NewRouter()
+	r.Post("/update/{metricType}/{metricName}/{metricValue}", requestLogger(stor.HandleMetricUpdate))
 	r.Post("/update", stor.HandleMetricUpdateJSON)
 	r.Post("/update/", stor.HandleMetricUpdateJSON)
 	r.Post("/value", stor.HandleValueJSON)
 	r.Post("/value/", stor.HandleValueJSON)
-    r.Get("/value/{metricType}/{metricName}", requestLogger(stor.HandleMetricGet))
-    r.Get("/", requestLogger(stor.HandleMain))
+	r.Get("/value/{metricType}/{metricName}", requestLogger(stor.HandleMetricGet))
+	r.Get("/", requestLogger(stor.HandleMain))
 
-    fmt.Printf("Server started at http://%s\n", addr)
-    err := http.ListenAndServe(addr, r)
-    if err != nil {
-        panic(err)
-    }
+	fmt.Printf("Server started at http://%s\n", addr)
+	err := http.ListenAndServe(addr, r)
+	if err != nil {
+		panic(err)
+	}
 }
