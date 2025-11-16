042597becf60cdc7cf6f8e653184aad942cfd852
Kilovoltov <kilovoltov@yandex.ru>
2025-11-16 23:24:14 +0500

    gets request body content lenghs

--- cmd/server/gzip.go
+++ cmd/server/gzip.go
@@ -94,7 +94,7 @@ func gzipMiddleware(h http.HandlerFunc) http.HandlerFunc {
 		// проверяем, что клиент отправил серверу сжатые данные в формате gzip
 		contentEncoding := r.Header.Get("Content-Encoding")
 		sendsGzip := strings.Contains(contentEncoding, "gzip")
-		if sendsGzip {
+		if sendsGzip && r.ContentLength >= 0 {
 			// оборачиваем тело запроса в io.Reader с поддержкой декомпрессии
 			cr, err := newCompressReader(r.Body)
 			if err != nil {
