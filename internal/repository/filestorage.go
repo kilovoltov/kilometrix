491fcce05fe856d0f9be18d1df52ee09b69b1015
Kilovoltov <kilovoltov@yandex.ru>
2025-12-04 22:24:49 +0500

    fixup

--- internal/repository/filestorage.go
+++ internal/repository/filestorage.go
@@ -152,6 +152,7 @@ func (f *FileStorage) LoadFromFile() error {
 	fmt.Printf("+++++++++++++++++ Loading from file: %s\n", f.filepath)
 	file, err := os.Open(f.filepath)
 	if err != nil {
+		fmt.Println("+++++++++ Something goes wrong!!!")
 		if os.IsNotExist(err) {
 			return err
 		}
